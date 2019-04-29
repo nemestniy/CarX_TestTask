ba1a997458be9cd246f8a16daaebca373fb38767
Max Tushkevich <maxonchik174@gmail.com>
2019-04-30 01:23:28 +0300

    Update Spawner.cs
     - Add methood CreateMonster()

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,8 +6,20 @@ public class Spawner : MonoBehaviour {
     [SerializeField]
 	private float m_interval = 3;
 
+
+    [Header("Monster Properties:")]
     [SerializeField]
-	private Transform m_moveTarget;
+    private PrimitiveType m_monsterType;
+
+    [SerializeField]
+    private Transform m_moveTarget;
+
+    [SerializeField]
+    private float m_speed;
+
+    [SerializeField]
+    private int m_maxHP;
+
 
 	private float m_timer;
 
@@ -21,12 +33,17 @@ public class Spawner : MonoBehaviour {
         m_timer -= Time.deltaTime;
 
 		if (m_timer <= 0) {
-			var newMonster = GameObject.CreatePrimitive (PrimitiveType.Capsule);
-			newMonster.transform.position = transform.position;
-			var monsterBeh = newMonster.AddComponent<Monster> ();
-			monsterBeh.ChangeMoveTarget(m_moveTarget);
 
+            CreateMonster(m_monsterType, m_maxHP, m_speed, m_moveTarget);
 			m_timer = m_interval;
 		}
 	}
+
+    private void CreateMonster(PrimitiveType monsterType, int HP, float speed, Transform moveTarget)
+    {
+        var newMonster = GameObject.CreatePrimitive(monsterType);
+        newMonster.transform.position = transform.position;
+        var monsterBeh = newMonster.AddComponent<Monster>();
+        monsterBeh.Init(HP, speed, moveTarget.position);
+    }
 }
