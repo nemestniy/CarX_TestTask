787d11f49a825dd6356fa7ecaf5e1f09cdce8d3b
Max Tushkevich <maxonchik174@gmail.com>
2019-04-30 01:22:46 +0300

    Update Monster.cs
    - Softening of movement
    - Add Monster initialization
    - Creating monsters in methood CreateMonster

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -5,26 +5,22 @@ public class Monster : MonoBehaviour {
 
     const float m_reachDistance = 0.3f;
 
-    [SerializeField]
-	private Transform m_moveTarget;
-
-    [SerializeField]
-    private float m_speed = 0.1f;
-
-    [SerializeField]
-    private int m_maxHP = 30;
-
-	private int m_hp;
+    private float m_speed;
+    private int m_maxHP;
     private Vector3 m_translation;
+    private Vector3 m_moveTarget;
 
-	void Start() {
-		m_hp = m_maxHP;
-	}
+    public void Init(int maxHP, float speed, Vector3 targetPosition)
+    {
+        m_maxHP = maxHP;
+        m_speed = speed;
+        m_moveTarget = targetPosition;
+    }
 
     public void DecreaseHP(int damage)
     {
-        m_hp -= damage;
-        if (m_hp <= damage)
+        m_maxHP -= damage;
+        if (m_maxHP <= damage)
             Destroy(gameObject);
     }
 
@@ -32,25 +28,22 @@ public class Monster : MonoBehaviour {
 		if (m_moveTarget == null)
 			return;
 		
-		if (Vector3.Distance (transform.position, m_moveTarget.position) <= m_reachDistance) {
+		if (Vector3.Distance (transform.position, m_moveTarget) <= m_reachDistance) {
 			Destroy (gameObject);
 			return;
 		}
 
-		m_translation = m_moveTarget.transform.position - transform.position;
+        m_translation = (m_moveTarget - transform.position) * m_speed;
 		if (m_translation.magnitude > m_speed) {
 			m_translation = m_translation.normalized * m_speed;
 		}
-		transform.Translate (m_translation);
+		transform.Translate (m_translation * Time.deltaTime);
+
 	}
 
     public Vector3 GetDirection()
     {
-        return (m_moveTarget.transform.position - transform.position).normalized;
+        return (m_moveTarget - transform.position).normalized;
     }
 
-    public void ChangeMoveTarget(Transform target)
-    {
-        m_moveTarget = target;
-    }
 }
